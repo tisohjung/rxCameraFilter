66b69b0f0c4b7e3f7578c8e3bddb10aa5c41483a
Minho Yi <tisohjung@gmail.com>
2021-05-15 22:41:39 +0900

    Request Photo library access

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -5,9 +5,20 @@
 //  Created by minho on 2021/05/15.
 //
 
-import Foundation
+import Photos
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
 
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization({ status in
+            if status == .authorized {
+                // access photo library
+            }
+        })
+    }
 }
