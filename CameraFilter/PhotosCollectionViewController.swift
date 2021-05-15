9445876dddfea7be971d0b17b9006d60d9bfc09e
Minho Yi <tisohjung@gmail.com>
2021-05-16 01:00:00 +0900

    get images from photo library

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -9,15 +9,23 @@ import Photos
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
+    private var images: [PHAsset] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         populatePhotos()
     }
 
     private func populatePhotos() {
-        PHPhotoLibrary.requestAuthorization({ status in
+        PHPhotoLibrary.requestAuthorization({ [weak self] status in
             if status == .authorized {
                 // access photo library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assets.enumerateObjects { object, _, _ in
+                    self?.images.append(object)
+                }
+                self?.images.reverse()
+                print(self?.images)
+//                self?.collectionView.reloadData()
             }
         })
     }
