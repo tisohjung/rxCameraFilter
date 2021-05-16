6c98a9a6f1dd42d7772f099579dd312c28e0efe8
Minho Yi <tisohjung@gmail.com>
2021-05-17 01:00:11 +0900

    select photo as with subject

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import Photos
+import RxSwift
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
+
     private var images: [PHAsset] = []
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,15 +41,17 @@ class PhotosCollectionViewController: UICollectionViewController {
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.images.count
+        return images.count
     }
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
             fatalError("PhotoCollectionViewCell is not found")
         }
 
-        let asset = self.images[indexPath.row]
+        let asset = images[indexPath.row]
         let manager = PHImageManager.default()
 
         manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: PHImageContentMode.aspectFit, options: nil) { image, _ in
@@ -54,5 +62,20 @@ class PhotosCollectionViewController: UICollectionViewController {
         }
 
         return cell
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedAsset = images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: PHImageContentMode.aspectFit, options: nil, resultHandler: { [weak self] image, info in
+            guard let info = info else { return }
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+
+            if !isDegradedImage {
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        })
     }
 }
